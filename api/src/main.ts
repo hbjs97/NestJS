546976fed6145f2b2362d345c1dae8b885ccfd5d
hbjs97 <hbjs97@naver.com>
2021-01-10 20:13:25 +0900

    fix, change port

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,12 +1,6 @@
-import {
-  ValidationPipe
-} from '@nestjs/common';
-import {
-  NestFactory
-} from '@nestjs/core';
-import {
-  AppModule
-} from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,8 +9,8 @@ async function bootstrap() {
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
-    })
+    }),
   );
-  await app.listen(3000);
+  await app.listen(4100);
 }
 bootstrap();
